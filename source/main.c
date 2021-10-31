6fd18013807beb797bd5c08b3ab6afa9c0a7df4f
Cindy Ho <57569111+cho102@users.noreply.github.com>
2021-10-31 15:49:49 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -36,8 +36,9 @@ void PWM_off() {
 	TCCR3B = 0x00;
 }
 
-enum States{Start, Init, C_Press, D_Press, E_Press}state;
+enum States{Start, Init, Power, Increment, Decrement}state;
 unsigned char counter = 0;
+unsigned char power = 0;
 void Tick() {
 	switch(state) {
 		case Start:
@@ -45,37 +46,39 @@ void Tick() {
 			break;
 		case Init :
 			if ((~PINA & 0x07) == 0x01) { 
-				state = C_Press; 
+				state = Power; 
 			}
-			else if ((~PINA & 0x07) == 0x02) { 
-				state = D_Press; 
+			else if (((~PINA & 0x07) == 0x02) && power == 1) { 
+				state = Increment; 
 			}
-			else if ((~PINA & 0x07) == 0x04) { 
-				state = E_Press; 
+			else if (((~PINA & 0x07) == 0x04) && power == 1) { 
+				state = Decrement; 
 			}
 			else { 
 				state = Init; 
 			}
 			break;
-		case C_Press:
+		case Power:
  			if ((~PINA & 0x07) == 0x01) { 
-				state = C_Press; 
+				state = Power; 
 			}
 			else { 
 				state = Init; 
 			}
 			break;
-		case D_Press:
-			if ((~PINA & 0x07) == 0x02) { 
-				state = D_Press; 
+		case Increment:
+			if (((~PINA & 0x07) == 0x02) && counter < 9) { 
+				++counter;
+				state = Increment; 
 			}
 			else { 
 				state = Init; 
 			}
 			break;
-		case E_Press:
-			if ((~PINA & 0x07) == 0x04) { 
-				state = E_Press; 
+		case Decrement:
+			if (((~PINA & 0x07) == 0x04) && counter > 0) { 
+				++counter;
+				state = Decrement; 
 			}
 			else { 
 				state = Init; 
@@ -90,14 +93,70 @@ void Tick() {
 		case Init:
 			set_PWM(0);
 			break;
-		case C4:
-			set_PWM(261.63);
+		case Power:
+			if (power == 0) {
+				power = 1;
+			}
+			else {
+				power = 0;
+			}
+			
 			break;
-		case D4:
-			set_PWM(293.66);
+		case Increment:
+			if (power == 1) {
+				if (counter == 1) {
+					set_PWM(261.63);
+				} 
+				else if (counter == 2) {	
+					set_PWM(293.66);
+				}
+				else if (counter == 3) {
+					set_PWM(329.63);
+				}
+				else if (counter == 4) {	
+					set_PWM(349.23);
+				}
+				else if (counter == 5) {
+					set_PWM(392.00);
+				}
+				else if (counter == 6) {	
+					set_PWM(440.00);
+				}
+				else if (counter == 7) {
+					set_PWM(493.88);
+				}
+				else if (counter == 8) {
+					set_PWM(523.25);
+				}
+			}
 			break;
-		case E4:
-			set_PWM(329.63);
+		case Decrement:
+			if (power == 1) {
+				if (counter == 1) {
+					set_PWM(261.63);
+				} 
+				else if (counter == 2) {	
+					set_PWM(293.66);
+				}
+				else if (counter == 3) {
+					set_PWM(329.63);
+				}
+				else if (counter == 4) {	
+					set_PWM(349.23);
+				}
+				else if (counter == 5) {
+					set_PWM(392.00);
+				}
+				else if (counter == 6) {	
+					set_PWM(440.00);
+				}
+				else if (counter == 7) {
+					set_PWM(493.88);
+				}
+				else if (counter == 8) {
+					set_PWM(523.25);
+				}
+			}
 			break;
 		default:
 			break;
